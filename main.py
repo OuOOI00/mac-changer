b0835f41e6f6c651f8f392cb912b488098d90928
TRIFOBO <111561716+OuOOI00@users.noreply.github.com>
2022-10-17 21:11:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import re
 
 def get_arguments():
     parser = opt.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="\n-i/--interface is your parametr of interface ")
-    parser.add_option("-m", "--mac", dest="new_mac", help="\n-m/new_mac is your parametr of new Mac address")
+    parser.add_option("-i", "--interface", dest="interface", help="\n-i/--interface is your parameter of interface ")
+    parser.add_option("-m", "--mac", dest="new_mac", help="\n-m/new_mac is your parameter of new Mac address")
     (options, arguments) = parser.parse_args()
 
     if not options.interface and not options.new_mac:
@@ -18,11 +18,11 @@ def get_arguments():
     return options
 
 def mc_changer(interface,new_mac):
-    print("[+] eth0 Mac address " + interface + " changed to " + new_mac)
+    print("[+] Mac address " + interface + " changed to " + new_mac)
     sbp.call(["ifconfig", interface, "down"])
     sbp.call(["ifconfig", interface, "hw", "ether", new_mac])
     sbp.call(["ifconfig", interface, "up"])
-    43
+    
     
 def get_currentMC(interface):
     ifconfig_param = sbp.check_output(["ifconfig", interface])
@@ -35,12 +35,12 @@ def get_currentMC(interface):
 options = get_arguments()
 
 current_mac = get_currentMC(options.interface)
-print("Current eth0 Mac address = " + str(current_mac))
+print("Current Mac address = " + str(current_mac))
 
 mc_changer(options.interface,options.new_mac)
 
 current_mac = get_currentMC(options.interface)
 if current_mac == options.new_mac:
-    print("[+] Current eth0 Mac address = " + str(current_mac))
+    print("[+] Current Mac address = " + str(current_mac))
 else:
     print("[-] Mac address don't be change ")
